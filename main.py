b37fb29169f9ac6a9699b7fd8f58f4f96f2b6482
def <basc11111@gmail.com>
2022-02-23 15:34:04 +0300

    Import tg bot

--- main.py
+++ main.py
@@ -10,6 +10,16 @@ def start(m, res=False):
     bot.send_message(m.chat.id, 'Я на связи. Напиши мне что-нибудь )')
 
 
+@bot.message_handler(commands=["test"])
+def start(m, res=False):
+    bot.send_message(m.chat.id, 'test')
+
+
+@bot.message_handler(commands=["add"])
+def start(m, res=False):
+    bot.send_message(m.chat.id, 'add')
+
+
 def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
