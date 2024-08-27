d0b2339678df5faa88a96a2747e3f2ae2fe48c32
Александр Баскаков <basc11111@gmail.com>
2024-08-27 14:45:28 +0300

    feat (bot): Add tg bot start

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -1,13 +1,42 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using NLog.Extensions.Logging;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot;
 
 var configuration = new ConfigurationBuilder()
     .AddUserSecrets<Program>()
     .Build();
 
+var logger = LoggerFactory.Create(builder => builder.AddNLog()).CreateLogger<Program>();
+
+
 var token = configuration["Token"];
 
+if(token == null)
+{
+    logger.LogError("Token not found!");
+    return;
+}
 
-var logger = LoggerFactory.Create(builder => builder.AddNLog()).CreateLogger<Program>();
-logger.LogInformation("Program has started.");
+var bot = new TelegramBotClient(token);
+
+var receiverOptions = new ReceiverOptions
+{
+    AllowedUpdates = new[]
+    {
+        UpdateType.Message,
+        UpdateType.CallbackQuery
+    },
+    ThrowPendingUpdates = true
+};
+using var cts = new CancellationTokenSource();
+
+//bot.StartReceiving(UpdateHandler.Invoke, ErrorHandler.Invoke, receiverOptions, cts.Token);
+
+await bot.GetMeAsync();
+
+logger.LogInformation("Bot started");
+
+await Task.Delay(-1);
