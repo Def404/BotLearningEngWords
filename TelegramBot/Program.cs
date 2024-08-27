c708548446409a0b8cfc245fcdba633039ad3e25
Александр Баскаков <basc11111@gmail.com>
2024-08-27 14:42:25 +0300

    feat (log): Add nlog

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -1,7 +1,13 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
 
 var configuration = new ConfigurationBuilder()
     .AddUserSecrets<Program>()
     .Build();
 
 var token = configuration["Token"];
+
+
+var logger = LoggerFactory.Create(builder => builder.AddNLog()).CreateLogger<Program>();
+logger.LogInformation("Program has started.");
